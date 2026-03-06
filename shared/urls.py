afc40c79ece65983f7f5321b31fc9d9c7c27757e
Husayn <husaynmuhammadyahyo1606@gmail.com>
2026-03-06 15:24:30 +0500

    s

--- shared/urls.py
+++ shared/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from shared.views import home_page_view, category_page_view
+from shared.views import home_page_view, category_page_view, product_page_view, blog_page_view
 
 app_name = 'shared'
 
 urlpatterns = [
     path('',home_page_view, name='home'),
     path('category/',category_page_view, name='category'),
+    path('product/',product_page_view, name='product'),
+    path('blog/',blog_page_view, name='blog'),
 ]
--- shared/views.py
+++ shared/views.py
@@ -5,3 +5,9 @@ def home_page_view(request):
 
 def category_page_view(request):
     return render(request,'category.html')
+
+def product_page_view(request):
+    return render(request,'product.html')
+
+def blog_page_view(request):
+    return render(request,'blog.html')
